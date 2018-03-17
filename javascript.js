09bbd5126d8dfb367800fef88edec70edf6e379d
Tony Polanowski <obsideonslate@gmail.com>
2018-03-16 22:21:30 -0600

    adding changes

--- javascript.js
+++ javascript.js
@@ -17,15 +17,30 @@ function populateButton(searchArray, classToAdd, areaToAddTo){
 }
 
 $(document).on('click','.searchButton', function(){
+    $('#searches').empty();
     var type = $(this).data('type');
-    var queryURL ='http://api.giphy.com/v1/gifs/search?q='+ type +'&api_key= limit=10';
+    var queryURL ='http://api.giphy.com/v1/gifs/search?q='+ type +'&api_key=iJhodVWXLHYhm0DvYr8gfZEk4A87LLGV&limit=10';
     $.ajax({url:queryURL, method:'GET'})
-        .done(funciton(response){
-            for(var i = 0; i <response.data.length; i++){
+        .done(function(response){
+            for (var i = 0; i < response.data.length; i++){
                 var searchDiv = $('<div class="search-item">');
                 var rating = response.data[i].rating;
                 var p = $('<p>').text('Rating: '+rating);
-                var still = 
+                var animated = response.data[i].images.fixed_height.url;
+                var still = response.data[i].images.fixed_height_still.url;
+                var image = $('<img>');
+                image.attr('src', still);
+                image.attr('data-still', still);
+                image.attr('data-animated', animated);
+                image.attr('data-state', 'still');
+                image.addClass('searchImage');
+                searchDiv.append(p);
+                searchDiv.append(image);
+                $('#searches').append(searchDiv);
             }
-        })
-})
+        });
+});
+
+$('#addSearch').on('click', function(){
+    var newSearch = $('input').eq(0).val();
+});
